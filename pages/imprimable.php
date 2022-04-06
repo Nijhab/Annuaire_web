182564d141ffa8803dcefb2b96263a0ad1a47250
Dieubénie <kodjim15@gmail.com>
2022-04-06 18:23:36 +0200

    imprimer

--- pages/imprimable.php
+++ pages/imprimable.php
@@ -21,6 +21,7 @@ if(isset($_REQUEST['delete_id']))
 }
 	
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -30,22 +31,9 @@ if(isset($_REQUEST['delete_id']))
     <title>Document</title>
     <?php include 'style.php';?>
 </head>
-<body>
-<?php include 'menu_page.php';?>  
-      <h1 style="font-family: Algerian; text-align: center;">LISTE DES ARTICLES</h1>
-	
-	<div class="wrapper">
-	
-	<div class="container">
-			
-		<div class="col-lg-12">
-			<div class="col-lg-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                           <h3><a href="add.php"><span class="glyphicon glyphicon-plus"></span> Ajouter un article</a></h3>
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body imprimable">
+<body onload="window.print()">
+<h1 style="font-family: Bahnschrift SemiCondensed; text-align: center;">LISTE DES ARTICLES</h1>
+<div class="panel-body imprimable">
                             <div class="table-responsive">
                                 <table class="table table-striped table-bordered table-hover">
                                     <thead>
@@ -55,8 +43,6 @@ if(isset($_REQUEST['delete_id']))
 											<th>QUANTITE</th>
                                             <th>PRIX UNITAIRE</th>
                                             <th>PROVENANCE</th>
-                                            <th>MODIFIER</th>
-                                            <th>SUPPRIMER</th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -72,8 +58,6 @@ if(isset($_REQUEST['delete_id']))
 											<td><?php echo $row['quantite']; ?></td>
                                             <td><?php echo $row['prixUnitaire']; ?></td>
                                             <td><?php echo $row['provenance']; ?></td>
-                                            <td><a href="edit.php?update_id=<?php echo $row['code']; ?>" class="btn btn-warning">Modifier</a></td>
-                                            <td><a href="?delete_id=<?php echo $row['code']; ?>" class="btn btn-danger">Supprimer</a></td>
                                         </tr>
                                     <?php
 									}
@@ -83,25 +67,6 @@ if(isset($_REQUEST['delete_id']))
                             </div>
                             <!-- /.table-responsive -->
                         </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
-                </div>
-				
-		</div>
-		
-	</div>
-			
-	</div>
-    <div class="container">
-    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-  <a href="imprimable.php">
-  <button class="btn btn-primary me-md-2" type="button">Imprimer</button>
-  </a>
-    </div>
-    </div>
-    <br>
-      <?php include 'pied.php';?>
-      <?php include 'script.php';?>								
-	</body>
+                        <?php include 'script.php';?>
+                        </body>
 </html>
